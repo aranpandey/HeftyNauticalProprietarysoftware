6f9c5b6ef972c33c3203f43f57a7844e28480b84
aranpandey <aranpandey@gmail.com>
2020-07-15 18:07:57 +0000

    new file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from ship import *
 pygame.init()
 screen_info=pygame.display.Info()
 print(screen_info)
@@ -6,11 +7,18 @@ size=(width,height)=(screen_info.current_w,screen_info.current_h)
 clock=pygame.time.Clock()
 screen=pygame.display.set_mode(size)
 color= (30,0,30)
+Numlevels = 4
+Level = 1
+AsteroidCount = 3
 Player = Ship()
 def main():
-  
-  while True:
-    clock.tick(60)
+  while Level <= Numlevels:
+    clock.tick(60)\
+    for event in pygame.event.get():
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_RIGHT
+          Player.speed[0] = 10
+    Player.update()
     screen.fill(color)
     screen.blit(Player.image, Player.rect)
     pygame.display.flip()
